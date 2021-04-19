29e092fae27ae467f738e9171bf73e789bc6f9bb
Matthew <cameron.matt@outlook.com>
2021-04-19 11:05:09 +1000

    arender more stable. Added images to clothing

--- mc-react-app/src/components/Clothing.js
+++ mc-react-app/src/components/Clothing.js
@@ -5,11 +5,9 @@ function Clothing() {
 
         const [data, setData] = useState("")
         const  product  = useContext(ProductsContext)
-        //const currentProductCategory = route.match.params.category;
     
         useEffect(() => {
             //console.log(product)
-            //const productCategory = currentProductCategory;
             const selectedProduct = product.products.filter((item) => item.category === 'men clothing');
             //console.log(selectedProduct)
             setData(selectedProduct);
@@ -18,71 +16,16 @@ function Clothing() {
             //console.log("category", productCategory);
             //console.log("selected product:", selectedProduct);
           }, []);
-          
 
-    //console.log("prod", Products)
-
-    // const innerArray = Object.entries(data)
-    // console.log('innerarr', innerArray)
-
-    // const productsArray = innerArray.map(prod => (
-    //     <div  key={prod.id}>
-    //         <div>
-    //             <a>{prod.title}</a>
-    //             <img src={prod.image} alt={prod.title}/>  
-    //         </div>
-    //     </div>
-    // ))
-
-    //const catMens = innerArray.filter(([key,value]) => value === 'clothing')
-    //const catMens = innerArray.map(([key,value]) => value !== '')
-
-    //const catMens = innerArray.filter((products) =>
-    //innerArray[0][1][0].includes('clothing')
-    //)
-
-
-    //console.log('catmens', catMens)
-
-    
-    // const catMensObj = Object.fromEntries(catMens)
-
-    // console.log('catmens', catMensObj)
-
-    // let filteredClothing = Products.products.map( products => products.products)
-        
-    // //     {
-    // //     (products.products.category === 'men clothing'
-    // //     ); 
-    // // })
-    //  console.log('filteredClothing', filteredClothing)
-    //const [clothingProducts, setclothingProducts] = useState(Products);
-    
-    //useEffect(() => console.log("cloth", products)) 
-
-    // clothingProducts(() => {
-    //     setclothingProducts(
-    //         Products.filter((product) =>
-    //         product.category.includes('clothing')
-    //       )
-    //     );
-    //   }, []);
-
-    // const sortByCategory = () => {  
-    //     const sorted = [...clothingProducts].sort(product => {
-    //         return product.category.includes("clothing")
-    //     });
-    //     setclothingProducts(sorted);
-    //     console.log("cloves", sortByCategory)
-    //   };
 
         return ( 
             
-            <div> 
+            <div>
                 {data.map(prod => (
-                    <li>
+                    <div key={prod.id}>
                         {prod.title}
-                    </li>
+                        <img src={prod.image} alt={prod.title}/>
+                    </div>
                     ))}
             </div>
         
