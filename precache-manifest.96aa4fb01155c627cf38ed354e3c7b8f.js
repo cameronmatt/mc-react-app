50960c838104142d2a0d68c43e4a4d9d397abdad
Matthew <cameron.matt@outlook.com>
2021-05-04 14:32:58 +1000

    Updates

---
+++ precache-manifest.96aa4fb01155c627cf38ed354e3c7b8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3941904f414f2e76362da592614aa77b",
+    "url": "/mc-react-app/index.html"
+  },
+  {
+    "revision": "5ffe93fb2c8e0855e633",
+    "url": "/mc-react-app/static/css/main.7fdcda6d.chunk.css"
+  },
+  {
+    "revision": "dc97f9970e9c1864e9c5",
+    "url": "/mc-react-app/static/js/2.be9b8ccd.chunk.js"
+  },
+  {
+    "revision": "0c9e570efe22235b6e81",
+    "url": "/mc-react-app/static/js/3.99f8f78b.chunk.js"
+  },
+  {
+    "revision": "5ffe93fb2c8e0855e633",
+    "url": "/mc-react-app/static/js/main.917e6b9c.chunk.js"
+  },
+  {
+    "revision": "6256fb8fcb936340889d",
+    "url": "/mc-react-app/static/js/runtime-main.fc31f564.js"
+  },
+  {
+    "revision": "30eaf0dd8c7f85c13918d9f686ce85fb",
+    "url": "/mc-react-app/static/media/bluff.30eaf0dd.png"
+  },
+  {
+    "revision": "3f0fb246510b0f98b329e5aa11bd7f15",
+    "url": "/mc-react-app/static/media/cart.3f0fb246.png"
+  },
+  {
+    "revision": "76aa1342f8d37ae7381998bea2aa4938",
+    "url": "/mc-react-app/static/media/electronics.76aa1342.png"
+  },
+  {
+    "revision": "2b86f67fbb494b2c78d1fb9df10689bd",
+    "url": "/mc-react-app/static/media/jewelery.2b86f67f.png"
+  },
+  {
+    "revision": "fa6f7ecbde323c57a9c8b2e0f677c9e7",
+    "url": "/mc-react-app/static/media/menswear.fa6f7ecb.png"
+  },
+  {
+    "revision": "6bbf501896a0ea4ff8bb71a9e909cda9",
+    "url": "/mc-react-app/static/media/womenswear.6bbf5018.png"
+  }
+]);
